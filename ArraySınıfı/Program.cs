d688b9c383ee473df416538c166541e017913977
ozlem55 <65675763+ozlem55@users.noreply.github.com>
2022-02-19 18:27:31 +0300

    Initial commit

---
+++ ArraySınıfı/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ArraySınıfı
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //sort
+            int[] sayiDizisi = { 23, 12, 86, 72, 3, 11, 17 };
+            Console.WriteLine("*******Sırasız liste******");
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+            Console.WriteLine("*******Sıralı liste******");
+            Array.Sort(sayiDizisi);
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+
+            //clear
+            Console.WriteLine("*******Array Clear******");
+            Array.Clear(sayiDizisi, 2, 2);
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+
+            //Reverse
+            Console.WriteLine("*******Reverse******");
+            Array.Reverse(sayiDizisi);
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+            //IndexOf
+            Console.WriteLine("*******IndexOf******");
+            
+            Console.WriteLine(Array.IndexOf(sayiDizisi, 23));
+
+            //Resize
+            Console.WriteLine("*******Resize******");
+            Array.Resize<int>(ref sayiDizisi,9);
+            sayiDizisi[8] = 99;
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+
+        }
+    }
+}
